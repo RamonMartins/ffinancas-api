13acaf67c5824714cf2d259d4b7231611fa35948
Ramon Martins Ferreira <ramon7mf@gmail.com>
2025-12-03 13:25:19 -0300

    add requirements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import lancamentos
+from .settings import settings
 
 projeto = FastAPI(title="Ferreira Finanças API")
 
@@ -7,6 +8,16 @@ projeto = FastAPI(title="Ferreira Finanças API")
 @projeto.get("/")
 def root():
     return "Bem vindo ao Ferreira Finanças!"
+
+
+@projeto.get("/loca")
+def root():
+    if settings.ENVIRONMENT == "development":
+        return "Local atual é development"
+    elif settings.ENVIRONMENT == "production":
+        return "Local atual é production"
+    else:
+        return "Nenhum local identificado"
 
 
 projeto.include_router(lancamentos.roteador)
---
+++ settings.py
@@ -0,0 +1,17 @@
+# settings.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    # O pydantic Settings lida automaticamente com a leitura do .env
+
+    # 1. Variável de Ambiente: Padrão é 'development' se a variável não estiver definida
+    ENVIRONMENT: str = "development" 
+    
+    # 2. Exemplo de Variável de Conexão com DB
+    DATABASE_URL: str = "sqlite:///./prod.db"
+
+    # Define de onde carregar as variáveis (padrão do pydantic)
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
+# Cria a instância que será importada na aplicação principal
+settings = Settings()
