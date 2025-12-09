347c36ffe7af46d9ae96aa589105b1b2b6052764
Ramon Martins Ferreira <ramon7mf@gmail.com>
2025-12-09 14:11:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://ffinancas-web.up.railway.app",
     "https://ffinancas-web.up.railway.app",
+    "https://ffinancas-web.up.railway.app/lancamentos",
 ]
 
 app.add_middleware(
