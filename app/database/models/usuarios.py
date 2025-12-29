9e27f6d7295587c80bed7f0d0748c31f16675257
Ramon Martins Ferreira <ramon7mf@gmail.com>
2025-12-29 13:31:06 -0300

    refactor: add FastAPI Users para Credencial

--- app/database/models/usuarios.py
+++ app/database/models/usuarios.py
@@ -1,18 +1,16 @@
 # app/database/models/usuarios.py
 
 import uuid
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
 from sqlalchemy import ForeignKey, DateTime, UUID, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from app.database.base import Base
 
 
-class UsuarioModel(Base):
+class UsuarioModel(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "usuarios"
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    senha: Mapped[str] = mapped_column(nullable=False)
     nome: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     modified_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
